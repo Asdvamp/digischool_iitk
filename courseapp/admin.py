d334c4d2eb75e1b374525c02aa321196a9b6e33b
Varun <varunguguloth@gmail.com>
2022-03-26 01:55:34 +0530

    Edited Admin.py
    Edited admin.py for viewing data and models

--- courseapp/admin.py
+++ courseapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+admin.site.register(models.CLASS_COURSES_MAPPING)
+admin.site.register(models.AVAILABLE_COURSES)
--- loginapp/admin.py
+++ loginapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from . import models
+
+admin.site.register(models.USER_SIGNUP_DATABASE)
+admin.site.register(models.QUERY_DATABASE)
+admin.site.register(models.TEACHER_CODE_MAPPING)
